9f085634e7850130d4e68be7d753e53dfd5c0b26
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-05-19 12:29:31 +0200

    :bookmark: working   :bookmark:

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 let personne1 = prompt("Veuillez entrer un premier prénom :")
-if (personne1 != null) {
-  document.getElementById("demo").innerHTML =
-  "Bonjour ";
-}
 let personne2 = prompt("Veuillez entrer un deuxième prénom :")
-if (personne2 != null) {
-  document.getElementById("demo").innerHTML +=
-  "Bonjour" + personne2 + "Comment ça va ?";
-}
+
+
+console.log(personne1 + ":" + " Bonjour !");
+console.log(personne2 + ":" + " Bonjour" + personne1 + " Comment ça va ?");
+
+let reponse = prompt(personne2 + ":" + " Comment ça va ?");
+console.log(personne1 + ":" + reponse +  " Et toi ?");
+
+let reponse2 = prompt(personne2 + ":" + " ");
+
+
+
